4b59428fee14ae7ebb88623c890f230745b5e8b3
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2020-05-14 02:31:19 -0400

    Push to gi

---
+++ index.js
@@ -0,0 +1,19 @@
+// var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+// var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+// alert("Width: " + viewportWidth + " Height: " + viewportHeight);
+// // let image = this.document.getElementById('hero');
+
+// // changeImage(viewportWidth);
+
+// window.addEventListener('resize', function () {
+//     viewportWidth = window.innerWidth || document.documentElement.clientWidth;	
+//     console.log(viewportWidth);
+// }, false);
+
+// // function changeImage(viewportWidth){
+// //     if (viewportWidth > 1380) {
+// // 		image.setAttribute('src', 'images/hero-desktop.jpg')
+// // 	} else {
+// // 		image.setAttribute('src', 'images/hero-mobile.jpg')
+// // 	}
+// // }
